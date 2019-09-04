012ff124a27cba1e16a3ff6e53468ca81360eb12
gunwoo <gunwoo9202@naver.com>
2019-09-04 21:42:00 +0900

    Vuetify에 Font, Icon 사용 가능하도록 플러그인 추가.

--- weedy/frontend/src/plugins/vuetify.js
+++ weedy/frontend/src/plugins/vuetify.js
@@ -1,3 +1,5 @@
+import '@mdi/font/css/materialdesignicons.css';
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 
