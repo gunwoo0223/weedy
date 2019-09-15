3560a874b633c2cb1f1b25ea6ca8dd032ede3700
gunwoo <gunwoo9202@naver.com>
2019-09-15 20:20:20 +0900

    메인페이지 플로팅 버튼 클릭 이벤트 (페이지 이동)

--- weedy/frontend/src/views/main/Main.vue
+++ weedy/frontend/src/views/main/Main.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="main">
         <!-- 상단 검색 부분 -->
-        <v-card
-            class="pa-4"
+        <v-card 
+            id="create"
             flat
         >
             <v-toolbar
@@ -19,37 +19,105 @@
             </v-btn>
 
             </v-toolbar>
-        </v-card>
-        <!-- 바로가기 아이콘들이 들어가는 곳 -->
-        <v-card-text style="height:60vh" class="grey lighten-5">
-
         
-        </v-card-text>
+            <!-- 바로가기 아이콘들이 들어가는 곳 -->
+            <v-card-text style="height:75vh" class="grey lighten-5">
 
-        <!-- 우측 하단 버튼 생기는 곳 -->
-        <v-card-text style="position: relative">
-            <v-fab-transition>
-            <v-btn
-            absolute
-            dark
-            fab
-            bottom
-            right
-            color="pink"
+            
+            </v-card-text>
+
+            <!-- 우측 하단 플로팅 버튼 생기는 곳 -->
+            <v-speed-dial
+                v-model="fab"
+                bottom
+                right
+                transition="slide-y-reverse-transition"
             >
-            <v-icon>reorder</v-icon>
-            </v-btn>
-            </v-fab-transition>
-        </v-card-text>
+                <template v-slot:activator>
+                <v-btn
+                    v-model="fab"
+                    dark
+                    fab
+                    color="pink"
+                >
+                <v-icon>reorder</v-icon>
+                </v-btn>
+                </template>
+                <!-- <v-btn
+                    fab
+                    dark
+                    small
+                    color="green"
+                >
+                <v-icon>mdi-pencil</v-icon>
+                </v-btn> -->
+
+                <!-- 플로팅 버튼 클릭 시 나타나는 리스트카드 -->
+                <v-card
+                    class="shortList"
+                    max-width="500"
+                >
+                    <v-list>
+                    <v-list-item-group v-model="model">
+                        <v-list-item
+                        v-for="(item, i) in items"
+                        :key="i"
+                        >
+                        <v-list-item-icon>
+                            <v-icon v-text="item.icon"></v-icon>
+                        </v-list-item-icon>
+                        <v-list-item-content>
+                            <router-link :to="item.router">
+                            <v-list-item-title v-text="item.text"></v-list-item-title>
+                            </router-link>
+                        </v-list-item-content>
+                        </v-list-item>
+                    </v-list-item-group>
+                    </v-list>
+                </v-card>
+            </v-speed-dial>
+        </v-card>
     </div>
 </template>
 
 <script>
 export default {
-    
+    data: () => ({
+      items: [
+        {
+          icon: 'person',
+          text: '로그인',
+          router: '/login',
+        },
+        {
+          icon: 'question_answer',
+          text: '등록',
+          router: '/register',
+        },
+        {
+          icon: 'keyboard_arrow_down',
+          text: '카테고리',
+          router: '/category',
+        },
+        {
+          icon: 'keyboard_arrow_down',
+          text: '문의',
+          router: '/qna',
+        },        
+      ],
+      model: 1,
+    }),
 }
 </script>
 
 <style scoped>
-
+#create .v-speed-dial {
+  position: absolute;
+}
+#create .v-btn--floating {
+  position: relative;
+}
+#create .shortList {
+    right: 30px;
+}
 </style>
