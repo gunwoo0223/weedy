8547cbb979f5bf9273f5bd87d96821295bc27762
gunwoo <gunwoo9202@naver.com>
2019-09-23 02:02:54 +0900

    프론트와 백을 연결하기 위한 proxy 설정

--- weedy/frontend/vue.config.js
+++ weedy/frontend/vue.config.js
@@ -1,8 +1,24 @@
 module.exports = {
 
-    //frontend port번호 설정
-    devServer:{
-        port:7070
+
+    //vue파일에 console.log 입력하면 에러나는 것 방지.
+    chainWebpack: config => {
+        config.module.rules.delete('eslint');
+    },
+
+    devServer: {
+        //frontend port번호 설정
+        port:7070,
+        //frontend <-> backend 연결을 위한 proxy 설정
+        proxy: {
+            // '/': {
+            //     target: 'http://localhost:8080/'
+            // },
+            '/main': {
+                target: 'http://localhost:8080/main',
+                changeOrigin: true,
+            },
+        }
     }
 
     //proxy 설정하기
