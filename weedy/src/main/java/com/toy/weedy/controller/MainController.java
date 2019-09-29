f911ce5ae4868d8f0ae9f670fad79a4ba05e3b7a
gunwoo <gunwoo9202@naver.com>
2019-09-30 00:34:48 +0900

    Merger하다가 날라가고 없어진 코드 및 파일들 복구

--- weedy/src/main/java/com/toy/weedy/controller/MainController.java
+++ weedy/src/main/java/com/toy/weedy/controller/MainController.java
@@ -2,6 +2,8 @@ package com.toy.weedy.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -15,18 +17,28 @@ import com.toy.weedy.repository.MainRepository;
 @RestController
 public class MainController extends WebSecurityConfigurerAdapter{
 
+    private Logger logger = LoggerFactory.getLogger(MainController.class);
+
 	@Autowired
 	MainRepository mainRepository;
 	
 	//'/'로 들어온 url에 대해서는 Security 적용x.
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/").permitAll().anyRequest().authenticated().and().httpBasic();
+		logger.info("MainConroller configure method execute");
+		
+        http.authorizeRequests().antMatchers("/*").permitAll().anyRequest().authenticated().and().httpBasic();
 		super.configure(http);
 	}
 	
 	@RequestMapping("/")
+	public void test() throws Exception {
+		logger.info("MainConroller / path execute");
+	}
+	
+	@RequestMapping("/main")
 	public @ResponseBody List<Apps> appNames() throws Exception {
+		logger.info("MainConroller /main path execute");
 		return mainRepository.findAll();
 	}
 }
---
+++ weedy/src/main/java/com/toy/weedy/filter/CorsFilter.java
@@ -0,0 +1,48 @@
+package com.toy.weedy.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@WebFilter(urlPatterns = "/*", description = "CORS")
+public class CorsFilter implements Filter{
+
+    private Logger logger = LoggerFactory.getLogger(CorsFilter.class);
+    
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    	logger.info("Cors Filter init method execute");
+        Filter.super.init(filterConfig);
+    }
+    
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    	logger.info("Cors Filter doFilter method execute");
+    	
+        HttpServletResponse res = (HttpServletResponse) response;
+        
+        res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+        res.setHeader("Access-Control-Max-Age", "3600");
+        res.setHeader("Access-Control-Allow-Headers", "Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization");
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        
+        chain.doFilter(request, response);
+    }
+    
+    @Override
+    public void destroy() {
+    	logger.info("Cors Filter destroy method execute");
+    	
+        Filter.super.destroy();
+    }
+}
