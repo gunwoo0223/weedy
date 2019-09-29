a0ecae10762b9da5d881cf554ab4a58e8bf43887
gunwoo <gunwoo9202@naver.com>
2019-09-29 16:08:39 +0900

    불필요 코드주석

--- weedy/frontend/src/main.js
+++ weedy/frontend/src/main.js
@@ -16,20 +16,20 @@ new Vue({
 
 
 
-var mysql = require('mysql');
-// Connection 객체 생성 
-var connection = mysql.createConnection({
-  host: '54.180.145.129',
-  port: 3306,
-  user: 'admin',   
-  password: '',
-  database: 'weedy'  
-});  
-// Connect
-connection.connect(function (err) {   
-  if (err) {     
-    console.error('mysql connection error');     
-    console.error(err);     
-    throw err;   
-  } 
-});
+// var mysql = require('mysql');
+// // Connection 객체 생성 
+// var connection = mysql.createConnection({
+//   host: '54.180.145.129',
+//   port: 3306,
+//   user: 'admin',   
+//   password: '',
+//   database: 'weedy'  
+// });  
+// // Connect
+// connection.connect(function (err) {   
+//   if (err) {     
+//     console.error('mysql connection error');     
+//     console.error(err);     
+//     throw err;   
+//   } 
+// });
