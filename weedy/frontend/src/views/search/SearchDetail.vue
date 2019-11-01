26c28edc66e4af7be5e837d222d3f6ea69ecc1d5
gunwoo <gunwoo9202@naver.com>
2019-11-01 23:37:44 +0900

    v-card height 100vh 성공. 디자인은 계속 바꿔야함.

--- weedy/frontend/src/views/search/SearchDetail.vue
+++ weedy/frontend/src/views/search/SearchDetail.vue
@@ -1,12 +1,54 @@
 <template>
-<p>
-{{this.$route.params.id}}
-</p>
+  <v-app id="inspire">
+    <v-content>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col
+            cols="6"
+          >
+            <v-card
+              height="100vh">
+              <v-card-text>
+                <v-form>
+                  <v-text-field
+                    label="Login"
+                    name="login"
+                    prepend-icon="person"
+                    type="text"
+                  />
+
+                  <v-text-field
+                    id="password"
+                    label="Password"
+                    name="password"
+                    prepend-icon="lock"
+                    type="password"
+                  />
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer />
+                <v-btn color="primary">Login</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
 export default {
+    data: () => ({
     
+  }),
 }
 </script>
 
